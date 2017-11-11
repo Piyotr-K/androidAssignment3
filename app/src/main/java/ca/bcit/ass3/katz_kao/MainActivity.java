9970b8d5c375e0776be77ef82cf465e4f866702f
Piyotr-K <piyotrkao1998@Outlook.com>
2017-11-11 15:08:34 -0800

    Working on main comp

--- app/src/main/java/ca/bcit/ass3/katz_kao/MainActivity.java
+++ app/src/main/java/ca/bcit/ass3/katz_kao/MainActivity.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(tb);
 
         ListView list_events = (ListView) findViewById(R.id.list_events);
 
